d6fa36e862fc5da21a3574790e08c464a2d2e4d9
architaw <38454160+architaw@users.noreply.github.com>
2019-06-20 19:45:03 +0530

    Create Words_in_Reverse_Order.cpp
    Words in Reverse Order using Vector

---
+++ Words_in_Reverse_Order.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int numberOfCases;
+    cin >> numberOfCases;
+    
+
+    
+    for(int i=0; i<numberOfCases; i++)
+    
+    {
+        int numberOfWords;
+        cin >> numberOfWords;
+        
+        string s;
+        vector<string> v1;
+        
+        // ---------- Storing Elements in a Vector ----------------
+        
+        for(int j=0; j<numberOfWords; j++)
+        {
+            
+            cin >> s;
+            v1.push_back(s);
+            
+        }
+        
+        // ------------ Reverse in a vector ---------------------
+        
+    reverse(v1.begin(), v1.begin()+v1.size());
+       
+    for (int i = 0; i <v1.size(); i++) 
+    cout << v1[i] << " ";
+        // -------------- Printing a Vector ---------------------
+    cout << endl;    
+    }
+    
+    return 0;
+}
